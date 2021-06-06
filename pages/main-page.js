d4dda55feba0563cc334efc39fce8fe3edb8798f
kwd <pei10118888@gmail.com>
2021-06-06 17:43:46 +0900

    add blog and post page

--- pages/main-page.js
+++ pages/main-page.js
@@ -1,5 +1,6 @@
 import Cookie from "universal-cookie";
 import Layout from "../components/Layout";
+import Link from 'next/link'
 import { useRouter } from "next/router";
 const cookie = new Cookie();
 
@@ -11,6 +12,18 @@ export default function MainPage() {
   };
   return (
     <Layout title="Main Page">
+      <div className="mb-10">
+        <Link href="/blog-page">
+          <a className="bg-indigo-500 mr-8  hover:bg-indigo-600 text-white px-4 py-12 rounded">
+            Visit Blog by SSG + ISR
+          </a>
+        </Link>
+        <Link href="/task-page">
+          <a className="bg-gray-500 ml-8 hover:bg-gray-600 text-white px-4 py-12 rounded">
+            Visit Task by ISR + CSR
+          </a>
+        </Link>
+      </div>
       <svg
         onClick={logout}
         className="mt-10 cusrsor-pointer w-6 h-6"
